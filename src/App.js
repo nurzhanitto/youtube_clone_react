b29202a07c01132d835adfc865ccc02055a7079f
Nurzhan Ilyasov <nurzanilasov@gmail.com>
2023-10-31 16:57:47 +0600

    created NavBar header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material"
-import Feed from "./components/Feed";
-import VideoDetail from "./components/VideoDetail";
-import ChannelDetail from "./components/ChannelDetail";
-import SearchFeed from "./components/SearchFeed";
-import Navbar from "./components/Navbar";
+import { Navbar, Feed, VideoDetail, SearchFeed, ChannelDetail } from "./components";
 
 const App = () => (
     <BrowserRouter>
